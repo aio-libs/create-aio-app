ab5f067cd276ec7d94d7e530300a875e3847ce57
misha.gavela <Arfey17.mg@gmail.com>
2018-09-23 22:27:26 +0300

    fix: corrected typos

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	rm -rf some_new/
+	rm -rf project_new/
 	pip install .
 	create-aio-app project_new
 	cd project_new/ && docker-compose up project_new_app
--- create_aio_app/template_project/template_project/main/tests/test_views.py
+++ create_aio_app/template_project/template_project/main/tests/test_views.py
@@ -2,4 +2,4 @@ async def test_view(client) -> None:
     resp = await client.get('/')
 
     assert resp.status == 200
-    assert 'Create aio ms' in await resp.text()
+    assert 'Create aio app' in await resp.text()

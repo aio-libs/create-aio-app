eb2b78a975afee0740471580de27f2269d1fc470
Nikolay Novik <nickolainovik@gmail.com>
2018-10-07 00:14:37 +0300

    enable ci

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info < (3, 5):
 
 def package_files(directory):
     paths = []
-    for (path, directories, filenames) in os.walk(directory):
+    for (path, _, filenames) in os.walk(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths

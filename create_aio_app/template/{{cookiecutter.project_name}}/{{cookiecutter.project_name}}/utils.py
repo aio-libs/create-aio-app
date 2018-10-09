0eb99482c2270af53858261a7d26a41bdbdf2e8c
Filatov Pavel <triksrimer@gmail.com>
2018-10-09 23:22:40 +0300

    added wait script for postgres to actually be ready

--- create_aio_app/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/utils.py
+++ create_aio_app/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/utils.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import pathlib
 from typing import Any, Optional, List
+{%- if cookiecutter.use_postgres == 'y' %}
+import socket
+import time
+{%- endif %}
 
 import trafaret
 from aiohttp import web
@@ -57,3 +61,17 @@ def init_config(
 ) -> None:
     app['config'] = \
         get_config(config or ['-c', DEFAULT_CONFIG_PATH.as_posix()])
+
+
+{%- if cookiecutter.use_postgres == 'y' %}
+if __name__ == '__main__':
+    postgres = get_config(['-c', DEFAULT_CONFIG_PATH.as_posix()])['postgres']
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            s.connect((postgres['host'], postgres['port']))
+            s.close()
+            break
+        except socket.error:
+            time.sleep(0.1)
+{%- endif %}

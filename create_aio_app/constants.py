a82bc023590a2dd5c270e0c12b08a71e756b95cd
Filatov Pavel <triksrimer@gmail.com>
2018-10-10 23:49:27 +0300

    remove wait_script file if postgres not using postgres

--- create_aio_app/constants.py
+++ create_aio_app/constants.py
@@ -4,6 +4,7 @@ __all__ = [
     'TEMPLATE_DIR',
     'DATABASE_TEMPLATE_DIRS',
     'DATABASE_TEMPLATE_FILES',
+    'WAIT_SERVICES_FILES'
 ]
 
 
@@ -11,3 +12,4 @@ TEMPLATE_NAME = 'template_project'
 TEMPLATE_DIR = pathlib.Path(__file__).parent / TEMPLATE_NAME
 DATABASE_TEMPLATE_DIRS = ['users', 'migrations', ]
 DATABASE_TEMPLATE_FILES = ['alembic.ini', ]
+WAIT_SERVICES_FILES = ['utils/wait_script.py', ]

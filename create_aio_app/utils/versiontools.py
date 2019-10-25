4b1a403ac2ac9f776dc084acfeb5a4d5c07d7a5e
Conor OCallaghan <brioscaibriste@gmail.com>
2019-10-25 22:18:29 +0100

    Updating version_check function following comments
    Update versiontools.py
    
    Removing unneeded import
    
    Update versiontools.py
    
    Update urllib request to remove open and replace with Request
    
    Update versiontools.py
    
    Fixing indentation
    
    Update versiontools.py
    
    Fixing try except pass bandit issue

--- create_aio_app/utils/versiontools.py
+++ create_aio_app/utils/versiontools.py
@@ -1,6 +1,11 @@
 """Version tools set."""
 
 import os
+import re
+import click 
+import json
+import create_aio_app
+import urllib.request
 from typing import Callable, Optional, Union
 
 from setuptools_scm import get_version
@@ -36,3 +41,22 @@ def cut_local_version_on_upload(version):
 def get_self_version():
     """Calculate the version of the dist itself."""
     return get_version_from_scm_tag(local_scheme=cut_local_version_on_upload)
+
+
+def version_check():
+    """Checks if the current version of the application is the latest"""
+    PYPI_URL = "https://pypi.org/pypi/create-aio-app/json"
+
+    try: 
+        with urllib.request.Request(PYPI_URL) as response:
+            res = json.loads(response.read())
+
+        code_version_long = create_aio_app.__version__
+        code_version_installed = re.sub(r'([0-9].[0-9].[0-9]).*', r'\1',code_version_long)
+        code_version_latest = res['info']['version']
+
+        if code_version_installed < code_version_latest:
+            print ("You are running an old version", code_version_installed,"upgrade with ", end='')
+            click.secho('pip install --upgrade create-aio-app',fg='yellow')
+    except:
+        print ("Failed to retrieve version information")

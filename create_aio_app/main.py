2411ab8d579207c150ff461ac11f0c5b4aa85974
= <=>
2020-04-13 14:47:17 -0300

    fix: fixed ci issue by removing security check in unnecessary context

--- create_aio_app/main.py
+++ create_aio_app/main.py
@@ -4,10 +4,7 @@ from pathlib import Path
 import click
 import os
 
-from cookiecutter.exceptions import (
-    FailedHookException,
-    OutputDirExistsException
-)
+from cookiecutter.exceptions import FailedHookException, OutputDirExistsException
 from cookiecutter.main import cookiecutter
 
 from create_aio_app.utils.config import parse_arguments
@@ -19,26 +16,27 @@ echo = partial(click.echo, err=True)
 
 def show_commands(folder):
     try:
-        os.chdir(f'{folder}/')
-        os.system(f'make help')
+        # nosec
+        os.chdir(f"{folder}/")
+        os.system(f"make help")
     except expression as identifier:
         pass
 
 
 def main():
     args = parse_arguments()
-    template_path = str(parent / 'template')
+    template_path = str(parent / "template")
 
     kwargs = {}
 
-    if args.get('name'):
+    if args.get("name"):
         kwargs = {
-            'no_input': True,
-            'extra_context': {
-                'project_name': args.get('name'),
-                'use_postgres': 'n' if args.get('without_postgres') else 'y',
-                'use_redis': 'y' if args.get('redis') else 'n',
-                'use_uvloop': 'y' if args.get('uvloop') else 'n',
+            "no_input": True,
+            "extra_context": {
+                "project_name": args.get("name"),
+                "use_postgres": "n" if args.get("without_postgres") else "y",
+                "use_redis": "y" if args.get("redis") else "n",
+                "use_uvloop": "y" if args.get("uvloop") else "n",
             },
         }
 
@@ -46,19 +44,15 @@ def main():
         result = cookiecutter(template_path, **kwargs)
     except (FailedHookException, OutputDirExistsException) as exc:
         if isinstance(exc, OutputDirExistsException):
-            echo(click.style(
-                '\n\nDirectory with such name already exists!\n',
-                fg='red',
-            ))
+            echo(
+                click.style("\n\nDirectory with such name already exists!\n", fg="red",)
+            )
         return
 
     folder = Path(result).name
 
-    echo(click.style(
-        '\n\nSuccessfully generated!\n',
-        fg='bright_green',
-    ))
-    echo('cd ' + click.style(f'{folder}/', fg='bright_blue'))
+    echo(click.style("\n\nSuccessfully generated!\n", fg="bright_green",))
+    echo("cd " + click.style(f"{folder}/", fg="bright_blue"))
     show_commands(folder)
 
 

af03ab5ac35df92d525fad55843cf46f5bd2233d
misha.gavela <Arfey17.mg@gmail.com>
2021-12-27 02:16:34 +0200

    feat: changed view of index page

--- create_aio_app/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/main/views.py
+++ create_aio_app/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/main/views.py
@@ -1,15 +1,9 @@
 from typing import Dict
 
 import aiohttp_jinja2
-import markdown2
 from aiohttp import web
-
-from {{ cookiecutter.project_name }}.constants import PROJECT_DIR
 
 
 @aiohttp_jinja2.template('index.html')
 async def index(request: web.Request) -> Dict[str, str]:
-    with open(PROJECT_DIR / 'README.md') as f:
-        text = markdown2.markdown(f.read())
-
-    return {"text": text}
+    return {}
